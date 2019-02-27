19ebe9490171e4131b58b86dbcf16d69b754f476
Yui Terai <yuiterai@Yuis-MacBook-Pro.local>
2019-02-27 22:04:39 +0900

    cerated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,50 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 
-var flipCards = function(cardId) {
+var flipCard = function(cardId) {
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		}
-		else {
-			alert("Sorry, try again.");
-		}
-	}
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+	} 
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
-flipCards(0);
-flipCards(2);
+
+flipCard(0);
+flipCard(2);
